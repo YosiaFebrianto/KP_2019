2b89f6069db7bf79ca21e66906fab16c91eb083f
Yosia Febrianto <yosiafebrianto@gmail.com>
2019-04-02 16:08:34 +0700

    Initial commit

---
+++ app/src/main/java/edu/stts/apihelper/UtilsApi.java
@@ -0,0 +1,12 @@
+package edu.stts.apihelper;
+
+public class UtilsApi {
+
+    // 10.0.2.2 ini adalah localhost.
+    public static final String BASE_URL_API = "http://192.168.0.100/KP2019_API/index.php/";
+
+    // Mendeklarasikan Interface BaseApiService
+    public static BaseApiService getAPIService(){
+        return RetrofitClient.getClient(BASE_URL_API).create(BaseApiService.class);
+    }
+}
