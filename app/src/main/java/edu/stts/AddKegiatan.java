960feab7f74ea9c86174cdef393a26862480910f
richardsumampouw <richardsumampouw33@gmail.com>
2019-06-11 10:58:14 +0700

    add laporan

--- app/src/main/java/edu/stts/AddKegiatan.java
+++ app/src/main/java/edu/stts/AddKegiatan.java
@@ -1,46 +1,110 @@
 package edu.stts;
 
 import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.SharedPreferences;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+
+import static android.content.Context.MODE_PRIVATE;
 
 public class AddKegiatan extends Fragment {
 
     private DatePickerDialog datePickerDialog;
-    private SimpleDateFormat dateFormatter;
-    private EditText btDatePicker,etTgl,etAlamat;
-    private TextView tvstatus;
-    private Button btn;
-
+    private TimePickerDialog timePickerDialog;
+    private SimpleDateFormat dateFormatter,timeFormatter;
+    private EditText btDatePicker,btTimePicker,etTgl,etAlamat;
+    private TextView tvstatus,etLeaderName;
+    private Button btn,addKegiatan;
+    private RadioGroup jenis;
+    private RadioButton selectedRadioButton;
+    private DomainConfig domainConfig;
+    RequestQueue requestQueue;
+    SharedPreferences sp;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_add_kegiatan, container, false);
         dateFormatter = new SimpleDateFormat("dd-MM-yyyy", Locale.US);
+        timeFormatter = new SimpleDateFormat("HH:mm", Locale.US);
         btDatePicker = (EditText) view.findViewById(R.id.etTanggal);
+        btTimePicker = (EditText) view.findViewById(R.id.etWaktu);
         btDatePicker.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showDateDialog();
             }
         });
-        btn=view.findViewById(R.id.btn_save);
+        btTimePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showHourDialog();
+            }
+        });
+        addKegiatan = view.findViewById(R.id.btn_addkegiatan);
+        jenis = (RadioGroup) view.findViewById(R.id.radioGroup);
+        sp = this.getActivity().getSharedPreferences("Login", MODE_PRIVATE);
+//        StringRequest stringRequest = new StringRequest(Request.Method.POST, domainConfig.getDomain_local() + "/Laporan_API/jabatan", new Response.Listener<String>() {
+//            @Override
+//            public void onResponse(String response) {
+//                try {
+//                    JSONObject resJSON = new JSONObject(response);
+//                    String message = resJSON.getString("message").toString();
+//                    etLeaderName = getView().findViewById(R.id.namaLeader);
+//                    etLeaderName.setText(message);
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//
+//            }
+//        }, new Response.ErrorListener(){
+//            @Override
+//            public void onErrorResponse(VolleyError error) {
+//                Log.e("error", error.getMessage());
+//            }
+//        }){
+//            @Override
+//            protected Map<String, String> getParams() {
+//                Map<String, String> params = new HashMap<>();
+//                params.put("username", sp.getString("username", "Dummy"));
+//                return params;
+//            }
+//        };
+//        requestQueue.add(stringRequest);
+        etLeaderName = view.findViewById(R.id.namaLeader);
+        etLeaderName.setText(sp.getString("nama", "Dummy"));
+        btn=view.findViewById(R.id.btn_addkegiatan);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,6 +131,61 @@ public class AddKegiatan extends Fragment {
             tvstatus.setText("Edit Laporan Komsel");
             btn.setText("Save");
         }
+        sp = this.getActivity().getSharedPreferences("Login", MODE_PRIVATE);
+
+        addKegiatan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("Response : ","jalan gan");
+                if(jenis.getCheckedRadioButtonId()==-1)
+                {
+                    Toast.makeText(getActivity(), "Isi Jenis Kegiatan!", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    // get selected radio button from radioGroup
+                    int selectedId = jenis.getCheckedRadioButtonId();
+                    // find the radiobutton by returned id
+                    selectedRadioButton = (RadioButton) getView().findViewById(selectedId);
+                    final String komsel_id = sp.getString("id_komsel", "Dummy");
+                    final String jenis = selectedRadioButton.getText().toString();
+                    final String waktu = etTgl.getText().toString();
+                    Toast.makeText(getActivity(), waktu, Toast.LENGTH_SHORT).show();
+                    final String alamat = etAlamat.getText().toString();
+                    StringRequest stringRequest = new StringRequest(Request.Method.POST, domainConfig.getDomain_local() + "/Laporan_API/addLaporan", new Response.Listener<String>() {
+                        @Override
+                        public void onResponse(String response) {
+                            try {
+                                JSONObject resJSON = new JSONObject(response);
+                                String message = resJSON.getString("message").toString();
+                                Toast.makeText(getActivity(), message, Toast.LENGTH_SHORT).show();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+                        }
+                    }, new Response.ErrorListener(){
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            Log.e("error", error.getMessage());
+                        }
+                    }){
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<>();
+                            params.put("id", sp.getString("id", "Dummy"));
+                            params.put("komsel_id", komsel_id);
+                            params.put("jenis", jenis);
+                            params.put("waktu", waktu);
+                            params.put("alamat", alamat);
+                            return params;
+                        }
+                    };
+                    requestQueue.add(stringRequest);
+                }
+            }
+        });
+
         return view;
     }
 
@@ -86,5 +205,18 @@ public class AddKegiatan extends Fragment {
         },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
 
         datePickerDialog.show();
+    }
+    private void showHourDialog(){
+        Calendar newCalendar = Calendar.getInstance();
+        timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                Calendar newDate = Calendar.getInstance();
+                newDate.set(hourOfDay, minute);
+                btTimePicker.setText(timeFormatter.format(newDate.getTimeInMillis()));
+            }
+        },newCalendar.get(Calendar.HOUR),newCalendar.get(Calendar.MINUTE),true);
+
+       timePickerDialog.show();
     }
 }
